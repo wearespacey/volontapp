862047e7a9acadebeadb610e590f2f6e66944190
Tinaël <tdevresse@neuromedia.io>
2019-08-02 15:36:28 +0200

    Add Display

---
+++ backend/VolontApp.Models/Display.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VolontApp.Models
+{
+    public class Display
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public Coordinates Location { get; set; } = new Coordinates();
+
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
+        public DateTime? DateRemoved { get; set; } = DateTime.UtcNow;
+
+        public string PhoneNumber { get; set; }
+    }
+}
