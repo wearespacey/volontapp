ed752a5b298bbf9c36503bc42210f47a180343de
steph <stephanie.bemelmans@mic-belgique.be>
2019-08-02 17:11:28 +0200

    fix navigation bug

--- frontend/Glue/Glue/Glue/App.xaml.cs
+++ frontend/Glue/Glue/Glue/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Glue
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
