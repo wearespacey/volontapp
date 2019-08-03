23297a7ca98b2c2d62d7eb18f4cd218ec7011f6b
Tinaël <tdevresse@neuromedia.io>
2019-08-03 19:54:35 +0200

    Override methods to do lazy loadings and take relationships into account
    Resolves #10

--- backend/VolontApp.DAL/Repositories/CoordinatorRepository.cs
+++ backend/VolontApp.DAL/Repositories/CoordinatorRepository.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Session;
 using VolontApp.Models;
 
 namespace VolontApp.DAL.Repositories
@@ -6,5 +10,104 @@ namespace VolontApp.DAL.Repositories
     {
         public CoordinatorRepository(IDocumentStoreHolder documentStoreHolder) : base(documentStoreHolder)
         { }
+
+        public new string Create(Coordinator entity, string id)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            using (IDocumentSession session = this.Store.OpenSession())
+            {
+                if (entity.VolunteerIds != null)
+                {
+                    foreach (string volunteerId in entity.VolunteerIds)
+                    {
+                        var volunteer = session.Load<Volunteer>(volunteerId);
+                        if (volunteer == null)
+                        {
+                            entity.VolunteerIds.RemoveAll(vId => vId.Equals(volunteerId));
+                        }
+                    }
+                }
+
+                session.Store(entity, id);
+                session.SaveChanges();
+            }
+
+            return id;
+        }
+
+        public new async Task<string> CreateAsync(Coordinator entity, string id)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            using (IAsyncDocumentSession session = this.Store.OpenAsyncSession())
+            {
+                if (entity.VolunteerIds != null)
+                {
+                    foreach (string volunteerId in entity.VolunteerIds)
+                    {
+                        var volunteer = await session.LoadAsync<Volunteer>(volunteerId);
+                        if (volunteer == null)
+                        {
+                            entity.VolunteerIds.RemoveAll(vId => vId.Equals(volunteerId));
+                        }
+                    }
+                }
+
+                await session.StoreAsync(entity, id);
+                await session.SaveChangesAsync();
+            }
+            return id;
+        }
+        
+        public new Coordinator Read(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            using (IDocumentSession session = this.Store.OpenSession())
+            {
+                Coordinator coordinator = session
+                    .Include<Coordinator>(c => c.VolunteerIds)
+                    .Load<Coordinator>(id);
+
+                if (coordinator.VolunteerIds != null)
+                {
+                    coordinator.Volunteers = new List<Volunteer>();
+                    foreach (string volunteerId in coordinator.VolunteerIds)
+                    {
+                        coordinator.Volunteers.Add(session.Load<Volunteer>(volunteerId));
+                    }
+                    coordinator.VolunteerIds = null;
+                }
+
+                return coordinator;
+            }
+        }
+
+        public new async Task<Coordinator> ReadAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            using (IAsyncDocumentSession session = this.Store.OpenAsyncSession())
+            {
+                Coordinator coordinator = await session
+                    .Include<Coordinator>(c => c.VolunteerIds)
+                    .LoadAsync<Coordinator>(id);
+
+                if (coordinator.VolunteerIds != null)
+                {
+                    coordinator.Volunteers = new List<Volunteer>();
+                    foreach (string volunteerId in coordinator.VolunteerIds)
+                    {
+                        coordinator.Volunteers.Add(await session.LoadAsync<Volunteer>(volunteerId));
+                    }
+                    coordinator.VolunteerIds = null;
+                }
+
+                return coordinator;
+            }
+        }
     }
 }
