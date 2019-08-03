89ade1f0928893d8396a3b0fc9fae91348b8cab5
Tinaël <tdevresse@neuromedia.io>
2019-08-03 21:47:11 +0200

    Fix typo

--- backend/VolontApp.API/Controllers/CaseController.cs
+++ backend/VolontApp.API/Controllers/CaseController.cs
@@ -29,18 +29,18 @@ namespace VolontApp.API.Controllers
         }
 
         // GET: api/Case/5
-        [HttpGet("{caseId}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Case>> GetById(string id)
         {
             return Ok(await CaseRepository.ReadAsync(id));
         }
 
-        // GET: api/Case/ByMissingChild/5
-        [HttpGet("ByMissingChild/{caseId}")]
-        public async Task<ActionResult<Case>> GetByMissingChild(string id, [FromBody] string installId)
+        // GET: api/Case/ByChildAndCoordinator/5
+        [HttpGet("ByChildAndCoordinator/{childId}")]
+        public async Task<ActionResult<Case>> GetByChildAndCoordinator(string childId, [FromBody] string installId)
         {
             return Ok((await CaseRepository.ReadAllAsync())
-                .FirstOrDefault(c => c.MissingStatus != MissingStatus.Found && c.Child.Id == id && c.Coordinator.InstallId == installId));
+                .FirstOrDefault(c => c.MissingStatus != MissingStatus.Found && c.Child.Id == childId && c.Coordinator.InstallId == installId));
         }
 
         // POST: api/Case
@@ -51,7 +51,7 @@ namespace VolontApp.API.Controllers
         }
 
         // PUT: api/Case/5
-        [HttpPut("{caseId}")]
+        [HttpPut("{id}")]
         public async Task<ActionResult> Put(string id, [FromBody] Case value)
         {
             await CaseRepository.UpdateAsync(value, id);
@@ -59,22 +59,22 @@ namespace VolontApp.API.Controllers
         }
 
         // PUT: api/Case/5/AddVolunteer
-        [HttpPut("{caseId}/AddVolunteer")]
-        public async Task<ActionResult<Case>> AddVolunteer(string caseId, [FromBody] string installId)
+        [HttpPut("{id}/AddVolunteer")]
+        public async Task<ActionResult<Case>> AddVolunteer(string id, [FromBody] string installId)
         {
-            Case caseToUpDate = await CaseRepository.ReadAsync(caseId);
+            Case caseToUpDate = await CaseRepository.ReadAsync(id);
             Volunteer volunteerToAdd = await VolunteerRepository.ReadByInstallIdAsync(installId);
 
             if (caseToUpDate.VolunteerIds == null) caseToUpDate.VolunteerIds = new List<string>();
             caseToUpDate.VolunteerIds.Add(volunteerToAdd.Id);
 
-            await CaseRepository.UpdateAsync(caseToUpDate, caseId);
+            await CaseRepository.UpdateAsync(caseToUpDate, id);
 
-            return Ok(await CaseRepository.ReadAsync(caseId));
+            return Ok(await CaseRepository.ReadAsync(id));
         }
 
         // DELETE: api/ApiWithActions/5
-        [HttpDelete("{caseId}")]
+        [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(string id)
         {
             await CaseRepository.DeleteAsync(id);
